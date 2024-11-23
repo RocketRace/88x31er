53972f866a4f11b4524e2e7ebdaa4e21ec1eb547
RocketRace <oliviaspalmu@gmail.com>
2024-11-24 00:53:12 +0200

    Image generation

--- src/app/img/[index]/route.ts
+++ src/app/img/[index]/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Jimp } from "jimp";
 
 export const dynamic = 'force-static'
 
@@ -8,11 +9,22 @@ export async function GET(
   { params }: { params: Promise<{ index: string }> }
 ) {
   const index = (await params).index;
+  const input = Buffer.from(index, "base64url");
+  const image = new Jimp({ width: 88, height: 31, color: 0xffffffff });
+  for (let x = 0; x < 88; x++) {
+    for (let y = 0; y < 31; y++) {
+      const i = (y * 88 + x) * 3;
+      const rgb = (input[i] << 16) | (input[i + 1] << 8) | input[i + 2];
+      image.setPixelColor(rgb, x, y);
+    }
+  }
+  const jpeg = await image.getBuffer('image/jpeg');
   return new NextResponse(
-    `You schmorgled: ${index}`,
-    { headers: { 
-      'content-type': 'text/plain',
-      'cache-control': 'public, max-age=31536000, immutable',
-    } 
-  });
+    jpeg,
+    {
+      headers: {
+        'content-type': 'image/jpeg',
+        // 'cache-control': 'public, max-age=31536000, immutable',
+      }
+    });
 }
