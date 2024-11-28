04bc78f8555356718d6d85c59876c5f660e3fcef
RocketRace <oliviaspalmu@gmail.com>
2024-11-28 21:16:48 +0200

    for now, use testing rng

--- src/app/img/[index]/route.ts
+++ src/app/img/[index]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Jimp } from "jimp";
+import { randomBytes } from "crypto";
 
 export const dynamic = 'force-static'
 
@@ -9,12 +10,14 @@ export async function GET(
   { params }: { params: Promise<{ index: string }> }
 ) {
   const index = (await params).index;
-  const input = Buffer.from(index, "base64url");
+  // const input = Buffer.from(index, "base64url");
+  const input = randomBytes(65472 / 8);
   const image = new Jimp({ width: 88, height: 31, color: 0xffffffff });
   for (let x = 0; x < 88; x++) {
     for (let y = 0; y < 31; y++) {
       const i = (y * 88 + x) * 3;
-      const rgb = (input[i] << 16) | (input[i + 1] << 8) | input[i + 2];
+      // Not using bitwise ops because JS is bees
+      const rgb = input[i] * 2**24 + input[i + 1] * 2**16 + input[i + 2] * 2**8 + 0xff;
       image.setPixelColor(rgb, x, y);
     }
   }
